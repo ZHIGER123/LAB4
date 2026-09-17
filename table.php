cbb87f0543a273cb96e2c6f6a5425662de494321
Mansur  Mukhametzhanov <mansurlubitgusya@gmail.com>
2026-09-17 17:26:19 +0600

    Упр.6: приём cols/rows/color через POST, сохранение значений в форме

---
+++ table.php
@@ -0,0 +1,29 @@
+<?php
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $cols = abs((int) $_POST['cols']);
+    $rows = abs((int) $_POST['rows']);
+    $color = trim(strip_tags($_POST['color']));
+  }
+  $cols = (!empty($cols)) ? $cols : 10;
+  $rows = (!empty($rows)) ? $rows : 10;
+  $color = (!empty($color)) ? $color : 'yellow';
+?>
+<form action='<?= $_SERVER['REQUEST_URI'] ?>' method='POST'>
+  <label>Количество колонок: </label>
+  <br />
+  <input name='cols' type='text' value="<?= $cols ?>" />
+  <br />
+  <label>Количество строк: </label>
+  <br />
+  <input name='rows' type='text' value="<?= $rows ?>" />
+  <br />
+  <label>Цвет: </label>
+  <br />
+  <input name='color' type='text' value="<?= $color ?>" />
+  <br />
+  <br />
+  <input type='submit' value='Создать' />
+</form>
+<!-- Таблица -->
+<?php drawTable($cols, $rows, $color); ?>
+<!-- Таблица -->
