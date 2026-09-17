f63a86638bc0601cd4582aa59cc630104b6f6467
Mansur  Mukhametzhanov <mansurlubitgusya@gmail.com>
2026-09-17 17:25:06 +0600

    Упр.3: about.php/contact.php/calc.php сведены к содержимому области контента

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php
+  $postMaxSize = ini_get('post_max_size');
+  $unit = strtoupper(substr($postMaxSize, -1));
+  $number = (int) $postMaxSize;
+
+  switch ($unit) {
+    case 'G':
+      $size = $number * 1024 * 1024 * 1024;
+      break;
+    case 'M':
+      $size = $number * 1024 * 1024;
+      break;
+    case 'K':
+      $size = $number * 1024;
+      break;
+    default:
+      $size = $number;
+  }
+?>
+<h3>Адрес</h3>
+<p>123456 Москва, Малый Американский переулок 21</p>
+<h3>Задайте вопрос</h3>
+<form action='' method='post'>
+  <label>Тема письма: </label>
+  <br />
+  <input name='subject' type='text' size="50" />
+  <br />
+  <label>Содержание: </label>
+  <br />
+  <textarea name='body' cols="50" rows="10"></textarea>
+  <br />
+  <br />
+  <input type='submit' value='Отправить' />
+</form>
+<p>Максимальный размер отправляемых данных <?= $size ?> байт.</p>
